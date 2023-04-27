3ec9c670119ae5359b28fe6e612c9f290769d7e1
Matrix-X <hbj418@gmail.com>
2023-04-27 23:47:05 +0800

    feat(reservation): make reservation by chan mode

--- deploy/database/custom/seed/datadictionary.go
+++ deploy/database/custom/seed/datadictionary.go
@@ -2,10 +2,15 @@ package seed
 
 import (
 	"PowerX/internal/model"
+	"PowerX/internal/uc/powerx"
 	"gorm.io/gorm"
 )
 
+var UseCaseDD *powerx.DataDictionaryUseCase
+
 func CustomDataDictionary(db *gorm.DB) (data []*model.DataDictionaryType) {
+
+	UseCaseDD = powerx.NewDataDictionaryUseCase(db)
 
 	return data
 
