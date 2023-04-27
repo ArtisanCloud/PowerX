204c7f10469f3465c0203c4e3c06f28a1947f957
Matrix-X <hbj418@gmail.com>
2023-04-27 23:25:47 +0800

    refact(customer): model fields

--- internal/model/customerdomain/customer.go
+++ internal/model/customerdomain/customer.go
@@ -4,14 +4,10 @@ import (
 	"PowerX/internal/model/powermodel"
 )
 
-const SourceFromMP = "mini-program"
-const SourceFromOA = "official-account"
-const SourceFromWeWork = "wework"
-
 type ExternalId struct {
-	OpenIDInMiniProgram           string `gorm:"index"`
-	OpenIDInWeChatOfficialAccount string `gorm:"index"`
-	OpenIDInWeCom                 string `gorm:"index"`
+	OpenIdInMiniProgram           string `gorm:"index;comment:微信小程序OpenId" json:"openIdInMiniProgram"`
+	OpenIdInWeChatOfficialAccount string `gorm:"index;comment:微信公众号OpenId" json:"openIdInWeChatOfficialAccount"`
+	OpenIdInWeCom                 string `gorm:"index;comment:企业微信OpenId" json:"openIdInWeCom"`
 }
 
 const CustomerUniqueId = "mobile"
@@ -20,12 +16,12 @@ type Customer struct {
 	//Inviter     *Customer
 
 	powermodel.PowerModel
-	Name        string
-	Mobile      string `gorm:"unique"`
-	Email       string
-	InviterID   int64
-	Source      string
-	Type        string
-	IsActivated bool
+	Name        string `gorm:"comment:客户名称" json:"name"`
+	Mobile      string `gorm:"unique;comment:店长Id" json:"mobile"`
+	Email       string `gorm:"comment:邮箱地址" json:"email"`
+	InviterID   int64  `gorm:"comment:邀请方" json:"inviterID"`
+	Source      int    `gorm:"comment:注册来源" json:"source"`
+	Type        int    `gorm:"comment:类型：个人，企业" json:"type"`
+	IsActivated bool   `gorm:"comment:激活状态" json:"isActivated"`
 	ExternalId
 }
--- internal/model/customerdomain/lead.go
+++ internal/model/customerdomain/lead.go
@@ -8,13 +8,13 @@ type Lead struct {
 	//Inviter *Customer
 
 	powermodel.PowerModel
-	Name        string
-	Mobile      string `gorm:"unique"`
-	Email       string
-	InviterID   int64
-	Source      string
-	Status      int8
-	IsActivated bool
+	Name        string `gorm:"comment:客户名称" json:"name"`
+	Mobile      string `gorm:"unique;comment:店长Id" json:"mobile"`
+	Email       string `gorm:"comment:邮箱地址" json:"email"`
+	InviterID   int64  `gorm:"comment:邀请方" json:"inviterID"`
+	Source      int    `gorm:"comment:注册来源" json:"source"`
+	Type        int    `gorm:"comment:类型：个人，企业" json:"type"`
+	IsActivated bool   `gorm:"comment:激活状态" json:"isActivated"`
 	ExternalId
 }
 
