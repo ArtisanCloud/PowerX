3fd69273dbd4bb959bda563b428c9618ae7ff7ae
Matrix-X <hbj418@gmail.com>
2023-07-10 23:25:26 +0800

    feat(custom): add checkSystemTimezone

--- internal/uc/custom.go
+++ internal/uc/custom.go
@@ -2,7 +2,10 @@ package uc
 
 import (
 	"PowerX/internal/config"
+	fmt "PowerX/pkg/printx"
+	"github.com/golang-module/carbon/v2"
 	"gorm.io/gorm"
+	"time"
 )
 
 type CustomUseCase struct {
@@ -11,7 +14,31 @@ type CustomUseCase struct {
 
 func NewCustomUseCase(conf *config.Config) (uc *CustomUseCase, clean func()) {
 
+	uc = &CustomUseCase{}
+
+	// 需要打印当时系统的Timezone
+	uc.CheckSystem()
 	return uc, func() {
 
 	}
+}
+
+func (uc *CustomUseCase) CheckSystem() {
+
+	// 设置 Golang 的 time 包的默认时区
+	cst := time.FixedZone("CST", 8*60*60)
+	time.Local = cst
+
+	// 设置 Carbon 库的默认时区
+	strTimezone := "Asia/Shanghai"
+	carbon.SetTimezone(strTimezone)
+
+	// carbon 的timezone
+	carbonTimezone := carbon.Now().Timezone()
+	fmt.Dump("carbon timezone is :", carbonTimezone)
+
+	// 输出系统默认时区
+	defaultTimezone := time.Now().Location()
+	fmt.Dump("System default timezone is:", defaultTimezone.String())
+
 }
